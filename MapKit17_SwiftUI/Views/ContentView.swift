724493be6f7fa8ed00bce12ab9ae8ff41ee57538
Nourhene Krichene <nourhenekrichene@Nourhenes-MBP.lan>
2024-02-29 13:05:10 +0100

    Add action to Annotation

--- MapKit17_SwiftUI/Views/ContentView.swift
+++ MapKit17_SwiftUI/Views/ContentView.swift
@@ -35,6 +35,9 @@ struct ContentView: View {
                              .background(in: .capsule)
                              */
                             Image(systemName: "mappin")
+                                .onTapGesture {
+                                    print(">> tapped child")
+                                }
                         }
                         
                     } else {
