12119c78585b40205092db147959deece57a90e2
Nourhene Krichene <nourhenekrichene@Nourhenes-MBP.lan>
2024-02-29 11:44:20 +0100

    initialize new project

---
+++ MapKit17_SwiftUI/MapKit17_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit17_SwiftUIApp.swift
+//  MapKit17_SwiftUI
+//
+//  Created by Nourhene Krichene on 29/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapKit17_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
